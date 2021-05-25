85f8fe6e3018cb2caaba4c742a6217177735fefd
SValv <s.valverde146@googlemail.com>
2021-05-25 12:04:06 +0200

    25.5

---
+++ Installfile.R
@@ -0,0 +1,10 @@
+if (!require(tidyverse)){install.packages("tidyverse")}
+if (!require(topicmodels)){install.packages("topicmodels")}
+if (!require(stopwords)){install.packages("stopwords")}
+if (!require(tidytext)){install.packages("tidytext")}
+if (!require(DT)){install.packages("DT")}
+if (!require(purrr)){install.packages("purrr")}
+if (!require(wordcloud)){install.packages("pdftools")}
+if (!require(pacman)){install.packages("pacman")}
+if (!require(devtools)){install.packages("devtools")}
+devtools::install_github("sebastiansauer/pradadata")
